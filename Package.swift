c2868bd175b410f001e5346184f6833bc99e6054
Alexander Steiner <info@alexsteiner.de>
2020-02-29 13:54:31 +0100

    created new vapor project from template: https://github.com/vapor/template

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "GraphQLAndVaporBeta",
+    platforms: [
+       .macOS(.v10_15)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-beta"),
+        .package(url: "https://github.com/vapor/fluent--driver.git", from: "-beta")
+    ],
+    targets: [
+        .target(name: "App", dependencies: [
+            .product(name: "Fluent", package: "fluent"),
+            .product(name: "FluentDriver", package: "fluent--driver"),
+            . product(name: "Vapor", package: "vapor")
+        ]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: [
+            .target(name: "App"),
+            .product(name: "XCTVapor", package: "vapor"),
+        ])
+    ]
+)
---
+++ Sources/App/configure.swift
@@ -0,0 +1,16 @@
+import Fluent
+import FluentDriver
+import Vapor
+
+// configures your application
+public func configure(_ app: Application) throws {
+    // uncomment to serve files from /Public folder
+    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    
+
+    app.migrations.add(CreateTodo())
+
+    // register routes
+    try routes(app)
+}
